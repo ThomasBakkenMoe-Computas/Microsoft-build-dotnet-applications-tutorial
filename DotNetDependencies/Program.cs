9f2f47ade1196a9171dc771cd2b0b5a77080b16e
Thomas Bakken Moe <thom@computas.com>
2025-02-06 16:09:44 +0100

    Initial commit

---
+++ DotNetDependencies/Program.cs
@@ -0,0 +1,26 @@
+﻿using Humanizer;
+
+Console.WriteLine("Quantities:");
+HumanizeQuantities();
+
+int n1 = 1;
+
+float n2 = 1.0f;
+
+Console.WriteLine("\nDate/Time Manipulation:");
+HumanizeDates();
+
+static void HumanizeQuantities()
+{
+    Console.WriteLine("Thomas".ToQuantity(99));
+    Console.WriteLine("case".ToQuantity(1));
+    Console.WriteLine("case".ToQuantity(5));
+}
+
+static void HumanizeDates()
+{
+    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+    Console.WriteLine(TimeSpan.FromMinutes(9000).Humanize());
+}
