7f0212850684182f7bea662abd9613d80916e3d7
Thomas Bakken Moe <thom@computas.com>
2025-02-10 15:46:44 +0100

    Test Path, FileInfo, and Environment functionality

--- WorkWithDirectories/Program.cs
+++ WorkWithDirectories/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Collections.Generic;
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json")) {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+FileInfo info = new FileInfo(fileName);
+
+Console.WriteLine($"Full name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Data: {info.CreationTime}");
