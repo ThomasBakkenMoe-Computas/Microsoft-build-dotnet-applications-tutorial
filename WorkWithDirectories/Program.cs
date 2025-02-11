1593164bae31908c249525417f696848dd0a71ec
Thomas Bakken Moe <thom@computas.com>
2025-02-11 12:37:44 +0100

    Continued work

--- WorkWithDirectories/Program.cs
+++ WorkWithDirectories/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
@@ -8,7 +9,12 @@ Directory.CreateDirectory(salesTotalDir);
 
 var salesFiles = FindFiles(storesDirectory);
 
-File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+var salesJson = File.ReadAllText($"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
+var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+
+File.AppendAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", salesData.Total.ToString());
+
+Console.WriteLine(salesData.Total);
 
 IEnumerable<string> FindFiles(string folderName)
 {
@@ -26,6 +32,12 @@ IEnumerable<string> FindFiles(string folderName)
 
     return salesFiles;
 }
+
+class SalesTotal
+{
+    public double Total { get; set; }
+}
+
 
 //string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
 //
