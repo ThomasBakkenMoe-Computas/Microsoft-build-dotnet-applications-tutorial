515ff6ce99c48d417bac2f79cb92d60f496b8f1c
Thomas Bakken Moe <thom@computas.com>
2025-02-11 09:46:35 +0100

    Changes FindFiles to find all .json files

--- WorkWithDirectories/Program.cs
+++ WorkWithDirectories/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
+
 IEnumerable<string> FindFiles(string folderName)
 {
     List<string> salesFiles = new List<string>();
@@ -9,8 +13,8 @@ IEnumerable<string> FindFiles(string folderName)
 
     foreach (var file in foundFiles)
     {
-        // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json")) {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json") {
             salesFiles.Add(file);
         }
     }
@@ -18,15 +22,13 @@ IEnumerable<string> FindFiles(string folderName)
     return salesFiles;
 }
 
-var salesFiles = FindFiles("stores");
-
 foreach (var file in salesFiles)
 {
     Console.WriteLine(file);
 }
 
-string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
-
-FileInfo info = new FileInfo(fileName);
-
-Console.WriteLine($"Full name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Data: {info.CreationTime}");
+//string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+//
+//FileInfo info = new FileInfo(fileName);
+//
+//Console.WriteLine($"Full name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Data: {info.CreationTime}");
